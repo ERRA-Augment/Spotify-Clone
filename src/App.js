372149d84a622161c2e27bcf83e1cf113c2a9c0a
JoshuaShelswell <josh.shelswell@gmx.com>
2021-03-29 02:38:30 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
-import React from "react"
+import React, { useEffect, useState } from "react";
 import './App.css';
 import Login from './Login'
+import { getTokenFromUrl } from './spotify'
 
 
 function App() {
+
+  // Run code based on a given condition 
+
+
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+
+  // Stuff
+
+  const hash = getTokenFromUrl();
+  window.location.hash = "";
+
+  const _token = hash.access_token;
+
+  if (_token) {
+    setToken(_token)
+  }
+
+  console.log("TOKENFACE", token);
+
+  }, []);
+
+
   return (
     <div className="app">
 
-
+      {
+        token ? (
+          <h1>I am faced in</h1>
+        ) : ( 
         <Login />
+        )
+      }
 
 
+
+        
     </div>
   );
 }
--- src/spotify.js
+++ src/spotify.js
@@ -3,12 +3,31 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 const redirectUri = "http://localhost:3000/";
 const clientId = "23d3ba79e4744fe188354a5d9fe2ebd4";
 const scope = [
+
   "user-read-currently-playing",
   "user-read-recently-played",
   "user-read-playback-state",
   "user-top-read",
   "user-modify-playback-state",
+  
 ];
+
+export const getTokenFromUrl  = () => {
+  return window.location.hash
+  .substring(1)
+  .split('&')
+  .reduce((initial, item) => {
+    //#accessToken=mysupersecretkey&name=sonny
+    let parts = item.split('=');
+    initial[parts[0]] = decodeURIComponent(parts[1]);
+
+    return initial;
+
+
+  }, {});
+};
+
+
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope.join(
   "%20"
 )}&response_type=token&show_dialog=true`;
